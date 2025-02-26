cd86ee05b3bffca2c0866cbd3d0bb9addadbcd55
Nico Del Rosario <57257533+Nirioppai@users.noreply.github.com>
2025-02-26 10:17:07 +0800

    Update cockpit_lxc_setup.sh

--- cockpit_lxc_setup.sh
+++ cockpit_lxc_setup.sh
@@ -1,6 +1,7 @@
-pct exec 101 -- bash
-wget https://github.com/45Drives/cockpit-file-sharing/releases/download/v4.2.9-2/cockpit-file-sharing_4.2.9-2focal_all.deb
-wget https://github.com/45Drives/cockpit-navigator/releases/download/v0.5.10/cockpit-navigator_0.5.10-1focal_all.deb
-wget https://github.com/45Drives/cockpit-identities/releases/download/v0.1.12/cockpit-identities_0.1.12-1focal_all.deb
-apt install -y ./*.deb
-rm *.deb
+pct exec 101 -- bash -c "
+    wget https://github.com/45Drives/cockpit-file-sharing/releases/download/v4.2.9-2/cockpit-file-sharing_4.2.9-2focal_all.deb &&
+    wget https://github.com/45Drives/cockpit-navigator/releases/download/v0.5.10/cockpit-navigator_0.5.10-1focal_all.deb &&
+    wget https://github.com/45Drives/cockpit-identities/releases/download/v0.1.12/cockpit-identities_0.1.12-1focal_all.deb &&
+    apt install -y ./*.deb &&
+    rm *.deb
+"
